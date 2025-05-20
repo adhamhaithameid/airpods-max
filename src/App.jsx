9bb416e3a5e05d04f7a5f96eb1694b2af7f0936c
AdhamHaithamEid <adhamhaithameid@gmail.com>
2025-05-20 04:34:50 +0300

    initialized the env

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="text-center content-center bg-black text-white text-2xl font-bold underline w-screen h-screen">
+      hello world
+    </div>
+  );
+}
+
+export default App;
